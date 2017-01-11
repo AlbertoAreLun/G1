2262f84c6712350b79bcba6945a7451b1d695811
AlbertoAreLun <alberto-arelu@hotmail.com>
2017-01-11 17:56:50 +0100

    Definir funciona suma
    Def funcion suma

--- src/test/TestSuma.java
+++ src/test/TestSuma.java
@@ -1,15 +1,18 @@
 
- package test;
- 
- import static org.junit.Assert.*;
- 
- import org.junit.Test;
- 
- public class TestSuma {
- 
- 	@Test
- 	public void test() {
- 		fail("Not yet implemented");
- 	}
- 
- }
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSuma {
+
+	@Test
+	public void testSuma1() {
+		assertEquals(4, Suma(1,3));
+	}
+
+	private int Suma(int i, int j) {
+		return 1+3;
+	}
+}
